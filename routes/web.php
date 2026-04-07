3fede08317867cde634a7737dbf645508089eb2e
Mehedii-07 <mehedihasan2272002@gmail.com>
2026-04-07 12:31:36 +0600

    home page create

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Pest\Support\View;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/',[HomeController::class,'index']);
+
+Route::get('/', [HomeController::class,'index']);
