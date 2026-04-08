f2170918ebbe23f66acb7641775324c778789d1d
Mehedii-07 <mehedihasan2272002@gmail.com>
2026-04-09 03:00:44 +0600

    add

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,1 +1,12 @@
-<h1>this is category page</h1>
+@extends('layouts.main')
+
+@push('title')
+<title>Category</title>
+@endpush
+
+@section('content')
+
+<div class="container-fluid bf-light p-5">
+    <h1 class="text-center"><i class="fa-solid fa-layer-group"></i>Category</h1>
+</div>
+@endsection
